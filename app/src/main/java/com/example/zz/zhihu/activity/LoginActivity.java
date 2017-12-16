2afad4851711cc0d6a84459cd7f76efd74a09c53
muxinxy <zeng5360@gmail.com>
2017-12-16 18:31:48 +0800

    删除背景图片，增加记住密码功能

--- app/src/main/java/com/example/zz/zhihu/activity/LoginActivity.java
+++ app/src/main/java/com/example/zz/zhihu/activity/LoginActivity.java
@@ -1,14 +1,17 @@
 package com.example.zz.zhihu.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,6 +21,9 @@ import com.example.zz.zhihu.R;
 public class LoginActivity extends AppCompatActivity {
     private MyDatabaseHelper dbHelper;
     private EditText login_username,login_password;
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private CheckBox rememberPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +36,18 @@ public class LoginActivity extends AppCompatActivity {
         final Button signup= findViewById(R.id.signup);
         login_username=findViewById(R.id.login_username);
         login_password=findViewById(R.id.login_passward);
+
+        pref= PreferenceManager.getDefaultSharedPreferences(this);
+        rememberPass=findViewById(R.id.remember_pass);
+        boolean isRemember=pref.getBoolean("remember_password",false);
+        if (isRemember){
+            String username=pref.getString("username","");
+            String password=pref.getString("password","");
+            login_username.setText(username);
+            login_password.setText(password);
+            rememberPass.setChecked(true);
+        }
+
         Toolbar toolbar = findViewById(R.id.toolbar_login);
         setSupportActionBar(toolbar);
         toolbar.setLogo(R.drawable.zhihu);
@@ -44,6 +62,15 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (check1()) {
                     if (check2()){
+                        editor=pref.edit();
+                        if (rememberPass.isChecked()){
+                            editor.putBoolean("remember_password",true);
+                            editor.putString("username",login_username.getText().toString());
+                            editor.putString("password",login_password.getText().toString());
+                        }else {
+                            editor.clear();
+                        }
+                        editor.apply();
                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                         intent.putExtra("username_intent",login_username.getText().toString().trim());
                         startActivity(intent);
